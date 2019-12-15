c744a90fb289e7bd6b4921a7697dfb87bb4faf2b
Maia <maia@Citadel.local>
2019-12-15 02:52:58 -0300

    Cleaning

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,106 +1,11 @@
-import Bouncer from "formbouncerjs";
-import Rellax from "rellax";
 import ScrollOut from "scroll-out";
-import messages from "./components/formMessages";
-
-let items = [];
-
-const validate = new Bouncer("form", {
-  messages: messages,
-  // Form Submission
-  disableSubmit: true, // If true, native form submission is suppressed even when form validates
-  // Custom Events
-  emitEvents: true // If true, emits custom events
-});
-// Detect a successful form validation
-document.addEventListener(
-  "bouncerFormValid",
-  function(e) {
-    // The successfully validated form
-    const form = e.target;
-    // If `disableSubmit` is true, you might use this to submit the form with Ajax
-    addItem(form);
-  },
-  false
-);
-
-function addItem(form) {
-  let item = {
-    id: Date.now(),
-    nome: form.elements.name.value,
-    email: form.elements.email.value
-  };
-  items.push(item);
-  form.reset();
-  localStorage.setItem("Items", JSON.stringify(items));
-  printItem(items);
-}
-
-const printItem = els => {
-  const theItems = els
-    .map(item => {
-      return `
-      <div class="item">
-        <h2>${item.nome}</h2>
-        <p>${item.email}</p>
-      </div>
-  `;
-    })
-    .join(" ");
-
-  const container = document.getElementById("container");
-  container.innerHTML = theItems;
-  document.body.append(container);
-};
-
-// ################################### PARALAX SIMPLES
-
-var hero = document.querySelector(".hero");
-if (hero) {
-  window.addEventListener("scroll", scroll);
-
-  function scroll() {
-    let scrolled = window.pageYOffset;
-
-    var coords = "0% " + (-(scrolled * 0.3) + "px");
-    hero.style.backgroundPosition = coords;
-
-    // hero.style.top = -(scrolled * 0.2) + "px";
-    // console.log(scrolled);
-  }
-}
-
-// ########################################### MODAL
-
-const openEls = document.querySelectorAll("[data-open]");
-const closeEls = document.querySelectorAll("[data-close]");
-const isVisible = "is-visible";
-
-for (const el of openEls) {
-  el.addEventListener("click", function() {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isVisible);
-  });
-}
-for (const el of closeEls) {
-  el.addEventListener("click", function() {
-    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
-  });
-}
-document.addEventListener("click", e => {
-  if (e.target == document.querySelector(".modal.is-visible")) {
-    document.querySelector(".modal.is-visible").classList.remove(isVisible);
-  }
-});
-
-// ########################################## RELLAX
-
-if (document.querySelector(".rellax")) {
-  const rellax = new Rellax(".rellax", {
-    center: true
-  });
-}
 
 // ########################################## SCROLL-OUT
 
-ScrollOut();
+ScrollOut({
+  // center: true,
+  cssProps: {
+    visibleY: true,
+    viewportY: true
+  }
+});
