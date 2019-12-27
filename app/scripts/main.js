7f39b9e8dfd023bc75a86b20e956b1cbf238adad
Maia <maia@Citadel.local>
2019-12-26 22:19:20 -0300

    Initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,14 @@
 import ScrollOut from "scroll-out";
+import Splitting from "splitting";
 
 // ########################################## SCROLL-OUT
 
 ScrollOut({
-  // center: true,
+  center: true,
   cssProps: {
     visibleY: true,
     viewportY: true
   }
 });
+
+Splitting({ target: ".content h2" });
